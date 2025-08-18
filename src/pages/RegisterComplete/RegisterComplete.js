68bf42da31f33789eeb11d645b3fff2b3eebe778
ishtiaq_ahmad <ishtiaqahmad427@gmail.com>
2025-08-18 22:05:11 +0500

    auth is dot

--- src/pages/RegisterComplete/RegisterComplete.js
+++ src/pages/RegisterComplete/RegisterComplete.js
@@ -29,19 +29,7 @@ const LogoSpinner = () => (
       <div className="logo-spinner-ring ring-2"></div>
       <div className="logo-spinner-ring ring-3"></div>
       <div className="logo-spinner-center">
-        <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          className="logo-spinner-icon"
-        >
-          <path
-            d="M12 2L15.09 8.26L22 9L17 14L18.18 21L12 17.77L5.82 21L7 14L2 9L8.91 8.26L12 2Z"
-            fill="currentColor"
-          />
-        </svg>
+        <div className="center-dot-auth"></div>
       </div>
     </div>
   </div>
